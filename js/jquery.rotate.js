6c95d27e106cdc27d55374885ecc86d8c77ec2ce
wendellvian <ybyqlong@126.com>
2013-10-30 12:30:35 +0800

    update

--- js/jquery.rotate.js
+++ js/jquery.rotate.js
@@ -1,67 +1,103 @@
-$(function(){
-    var oRotate = $(".rotate");
-    var oCtrlBo = $(".ctrlBo a");
-    var oCtrlPo = $(".ctrlPo");
-    var oLeftCtrl = $(".leftCtrlPo");
-    var oRightCtrl = $(".rightCtrlPo");
-    var oRotateUl = $(".rotate").find("ul").eq(0);
-    var oRotateLi = oRotateUl.find("li");
-    var vIndex = 0;
-    var leng = oRotateLi.length;
-    var timer = null;
+/*
+ * rotate v1.0
+ * Copyright (c) 2013 Wendell  http://blog.webql.info/
+*/
+(function($){
+    $.fn.extend({
+        rotate:function(options){
+            var defaults = {
+                mouseCtrl:"click",                     // 切换导航鼠标事件
+                ctrlEleName:"ctrlBo",                  // 切换导航
+                ctrlEleNamePo:"ctrlPo",                // 单向切换导航
+                position:"left",                       // 切换运动方向
+                speed:500,                             // 切换运动速度
+                aniTime:5000,                          // 自动切换停滞时间
+                focus:"on",                            // 当前焦点
+                focuSiblings:"a",                      // 当前焦点同辈的元素
+                eleName:"a",                           // 切换导航按扭元素名
+                leftCtrlClass:".leftCtrl",             // 单向左控制按扭
+                rightCtrlClass:".rightCtrl"            // 单向右控制按扭
+
+            }
+            var options = $.extend(defaults,options);
+            var thisObj = $(this);
+
+            var oRotate = thisObj;
+            var oRotateUl = thisObj.find("ul").eq(0);
+            var oRotateLi = oRotateUl.find("li");
+            var leng = oRotateLi.length;
+            var oCtrlBo = $("."+options.ctrlEleName).find(options.eleName);
+            var oCtrlPo = $("."+options.ctrlEleNamePo);
+            var vIndex = 0;
+            var timer = null;
+
+            switch(options.position){
+                case "left":
+                    oRotateUl.css("width",oCtrlBo.length * oRotateLi.width());
+                break;
+                case "top":
+                    oRotateLi.css({float:"none",display:"block"});
+                break;
+                case "opacity":
+                    oRotateLi.css({opacity:0,position:"absolute"});
+                    oRotateLi.eq(vIndex).css({opacity:1});
+                break;
+            }
+            
+            function movePosition(obj){
+                oCtrlBo.eq(obj).addClass(options.focus).siblings(options.focuSiblings).removeClass(options.focus);
+                switch(options.position){
+                    case "left":
+                        oRotateUl.stop().animate({left:-oRotateLi.width() * obj},options.speed);
+                    break;
+                    case "top":
+                        oRotateUl.stop().animate({top:-oRotateLi.height() * obj},options.speed);
+                    break;
+                    case "opacity":
+                        oRotateLi.eq(obj).stop().animate({opacity:1},options.speed).siblings().animate({opacity:0},options.speed);
+                    break;
+                }
+            }
+            function rotateAni(){
+                vIndex = $(this).index();
+                movePosition(vIndex);
+            }
+            var setAni = {
+                rightCtrl:function(){   // 单向右控制键
+                    if(vIndex == oCtrlBo.length - 1){
+                        vIndex = 0;
+                    }else{
+                        vIndex++;
+                    }
+                    movePosition(vIndex);
+                },
+                leftCtrl:function(){    // 单向左控制键
+                    if(vIndex == 0){
+                        vIndex = leng - 1;
+                    }else{
+                        vIndex--;
+                    }
+                    movePosition(vIndex);
+                }
+            }
+            function mOver(){
+                clearInterval(timer);
+                oCtrlPo.show();
+            }
+            function mOut(){
+                timer = setInterval(setAni.rightCtrl,options.aniTime);
+                oCtrlPo.hide();
+            }
+
+            oCtrlBo.bind(options.mouseCtrl,rotateAni);
+            timer = setInterval(setAni.rightCtrl,options.aniTime);
+            oRotate.bind("mouseover",mOver);
+            oRotate.bind("mouseout",mOut);
+            $(options.rightCtrlClass).bind("click",setAni.rightCtrl);
+            $(options.leftCtrlClass).bind("click",setAni.leftCtrl);
 
 
-    oRotateUl.css("width",oCtrlBo.length * oRotateLi.width());
-
-    function slideAni(){
-        $(this).addClass("on").siblings("a").removeClass("on");
-        oRotateUl.stop().animate({
-            left:-oRotateLi.width() * $(this).index()
-        },"slow");
-    }
-    function setAni(){
-        if(vIndex == oCtrlBo.length - 1){
-            vIndex = 0;
-        }else{
-            vIndex++;
+            return $(this);
         }
-        oCtrlBo.eq(vIndex).addClass("on").siblings("a").removeClass("on");
-        oRotateUl.stop().animate({
-            left:-oRotateLi.width() * vIndex
-        },"slow");
-    }
-    function mOver(){
-        clearInterval(timer);
-        oCtrlPo.show();
-    }
-    function mOut(){
-        timer = setInterval(setAni,3000);
-        oCtrlPo.hide();
-    }
-
-    oCtrlBo.bind("click", slideAni);
-    timer = setInterval(setAni,3000);
-    oRotate.bind("mouseover",mOver);
-    oRotate.bind("mouseout",mOut);
-
-    oRightCtrl.click(function(){
-    	oRotateUl.stop().animate({left:"-="+oRotateLi.width()},"slow");
-    	vIndex++;
-    	oCtrlBo.eq(vIndex).addClass("on").siblings("a").removeClass("on");
-    	if(vIndex>leng-1){
-    		oRotateUl.stop().animate({left:0},"slow");
-    		vIndex = 0;
-    		oCtrlBo.eq(vIndex).addClass("on").siblings("a").removeClass("on");
-    	}
     });
-    oLeftCtrl.click(function(){
-    	oRotateUl.stop().animate({left:"+="+oRotateLi.width()},"slow");
-    	vIndex--;
-    	oCtrlBo.eq(vIndex).addClass("on").siblings("a").removeClass("on");
-    	if(vIndex<0){
-    		vIndex = leng - 1;
-    		oRotateUl.stop().animate({left:oRotateLi.width() * -vIndex},"slow");
-    		oCtrlBo.eq(vIndex).addClass("on").siblings("a").removeClass("on");
-    	}
-    });
-});
+})(jQuery);
